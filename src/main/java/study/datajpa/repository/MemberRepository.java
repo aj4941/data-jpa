9ab69b6d5904b985cc8b755862c1664ccecfda99
aj4941 <51875177+aj4941@users.noreply.github.com>
2023-03-02 22:32:07 +0900

    dto로 조회하는 메서드 생성

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -16,4 +17,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     // 동적 쿼리는 Querydsl을 사용하는 것이 좋다.
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    // Querydsl로 편리하게 하는 방법이 존재
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
